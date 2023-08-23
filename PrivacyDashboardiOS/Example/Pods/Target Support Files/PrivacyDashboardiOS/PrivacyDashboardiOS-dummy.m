427d8ef2f8182a437b347a724d696d418dfd69b3
Mumthasir mohammed <>
2023-08-23 19:43:34 +0530

    Add #1: Initial commit

---
+++ PrivacyDashboardiOS/Example/Pods/Target Support Files/PrivacyDashboardiOS/PrivacyDashboardiOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PrivacyDashboardiOS : NSObject
+@end
+@implementation PodsDummy_PrivacyDashboardiOS
+@end
---
+++ PrivacyDashboardiOS/Example/PrivacyDashboardiOS/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PrivacyDashboardiOS
+//
+//  Created by Mumthasir mohammed on 08/24/2023.
+//  Copyright (c) 2023 Mumthasir mohammed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
