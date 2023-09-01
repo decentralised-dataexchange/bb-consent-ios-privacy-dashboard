a90ea36df592d5ffd7d14d9cd5928c1a10e62f7a
Mumthasir mohammed <>
2023-09-01 21:15:33 +0530

    #Add 11: Code formatted.

--- PrivacyDashboardiOS/PrivacyDashboardiOS/Classes/BBConsentKeychainUtils.swift
+++ PrivacyDashboardiOS/PrivacyDashboardiOS/Classes/BBConsentKeychainUtils.swift
@@ -42,18 +42,3 @@ class BBConsentKeyChainUtils {
         return swiftString
     }
 }
-
-extension Data {
-    init<T>(from value: T) {
-        var value = value
-        var myData = Data()
-        withUnsafePointer(to:&value, { (ptr: UnsafePointer<T>) -> Void in
-            myData = Data( buffer: UnsafeBufferPointer(start: ptr, count: 1))
-        })
-        self.init(myData)
-    }
-    
-    func to<T>(type: T.Type) -> T {
-        return self.withUnsafeBytes { $0.load(as: T.self) }
-    }
-}
