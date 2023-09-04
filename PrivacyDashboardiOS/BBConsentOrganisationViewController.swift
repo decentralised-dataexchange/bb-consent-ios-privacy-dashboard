58d2a5247ce62aad3148bdf45cf3078bd2d86b96
Mumthasir mohammed <unknown>
2023-09-04 20:20:12 +0530

    Add #12: Consent agreement home screen assets.

---
+++ PrivacyDashboardiOS/BBConsentOrganisationViewController.swift
@@ -0,0 +1,12 @@
+//
+//  OrganisationViewController.swift
+//  PrivacyDashboardiOS
+//
+//  Created by Mumthasir mohammed on 04/09/23.
+//
+
+import Foundation
+
+class BBConsentOrganisationViewController: UIViewController {
+    
+}
