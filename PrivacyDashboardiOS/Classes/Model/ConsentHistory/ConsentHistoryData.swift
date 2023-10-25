4906af8b2c51b1b76a3969c2e5400552bd9af376
Mumthasir mohammed <>
2023-10-25 12:18:22 +0530

    Add #100: Update consent history API to V2

--- PrivacyDashboardiOS/Classes/Model/ConsentHistory/ConsentHistoryData.swift
+++ PrivacyDashboardiOS/Classes/Model/ConsentHistory/ConsentHistoryData.swift
@@ -5,10 +5,9 @@
 import Foundation 
 import SwiftyJSON
 
-class ConsentHistoryData {
+class ConsentHistoryData: ConsentHistoryDataWrapperV2 {
 	var consentHistory : [ConsentHistory]?
-	var links : Link?
-    var unReadCount : Int?
+    var pagination: Pagination?
 
 	/**
 	 * Instantiate the instance using the passed json values to set the properties values
@@ -18,22 +17,57 @@ class ConsentHistoryData {
 			return
 		}
 		consentHistory = [ConsentHistory]()
-		let consentHistoryArray = json["ConsentHistory"].arrayValue
+		let consentHistoryArray = json["dataAgreementRecordHistory"].arrayValue
 		for consentHistoryJson in consentHistoryArray{
 			let value = ConsentHistory(fromJson: consentHistoryJson)
 			consentHistory?.append(value)
 		}
-		unReadCount = json["UnReadCount"].intValue
-        let linksJson = json["Links"]
-
-		if !linksJson.isEmpty{
-			links = Link(fromJson: linksJson)
-		}
+        let tempPagination = json["pagination"]
+        if !tempPagination.isEmpty {
+            pagination = Pagination(fromJson: tempPagination)
+        }
 	}
 }
 
+// Api version 1
 protocol ConsentHistoryDataWrapper {
     var consentHistory : [ConsentHistoryWrapper]? { get }
     var links : Link?  { get }
     var unReadCount : Int?  { get }
+}
+
+// Api version 2
+protocol ConsentHistoryDataWrapperV2 {
+    var consentHistory : [ConsentHistory]? { get }
+    var pagination: Pagination? { get }
+}
+
+class Pagination: PaginationWrapper {
+    var currentPage: Int?
+    var totalItems: Int?
+    var totalPages: Int?
+    var limit: Int?
+    var hasPrevious = false
+    var hasNext = false
+    
+    init(fromJson json: JSON!) {
+        if json.isEmpty{
+            return
+        }
+        currentPage = json["currentPage"].intValue
+        totalItems = json["totalItems"].intValue
+        totalPages = json["totalPages"].intValue
+        limit = json["limit"].intValue
+        hasPrevious = json["hasPrevious"].boolValue
+        hasNext = json["hasNext"].boolValue
+    }
+}
+
+protocol PaginationWrapper {
+    var currentPage: Int? { get }
+    var totalItems: Int? { get }
+    var totalPages: Int? { get }
+    var limit: Int? { get }
+    var hasPrevious: Bool { get }
+    var hasNext: Bool { get }
 }
