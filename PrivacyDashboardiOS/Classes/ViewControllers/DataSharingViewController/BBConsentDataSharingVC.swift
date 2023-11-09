66df2ea2adbf1637abc76db484baafe8d1083a3d
Mumthasir mohammed <unknown>
2023-11-09 22:50:12 +0530

    Add #140: Data sharing UI + functionality

---
+++ PrivacyDashboardiOS/Classes/ViewControllers/DataSharingViewController/BBConsentDataSharingVC.swift
@@ -0,0 +1,218 @@
+//
+//  BBConsentDataSharingVC.swift
+//  AFDateHelper
+//
+//  Created by Mumthasir mohammed on 09/11/23.
+//
+
+import Foundation
+import Kingfisher
+
+class BBConsentDataSharingVC: BBConsentBaseViewController, WebServiceTaskManagerProtocol, UITextViewDelegate {
+    // MARK: IBOutlets
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var labelStackView: UIStackView!
+    @IBOutlet weak var ourLogoImageView: UIImageView!
+    @IBOutlet weak var thierLogoImageView: UIImageView!
+    @IBOutlet weak var betweenLogosImageView: UIImageView!
+    @IBOutlet weak var textLabelOne: UILabel!
+    @IBOutlet weak var textLabelTwo: UILabel!
+    @IBOutlet weak var textLabelThree: UILabel!
+    @IBOutlet weak var dataAgreementTextView: UITextView!
+    
+    // MARK: Variables
+    var organizationData : Organization?
+    var organizationDetailsData : OrganisationDetails?
+    var dataAgreementId: String?
+    var theirLogoImageUrl: String?
+    var theirOrgName: String?
+    var termsOfServiceText: String?
+    var termsOFServiceUrl: String?
+    var cancelButtonText: String?
+
+    // MARK: Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+        callOrganizationApi()
+        callOrganisationDetailsApi()
+    }
+    
+    func setUI() {
+        overrideUserInterfaceStyle = .light
+        cancelButton.backgroundColor = .clear
+        cancelButton.layer.borderWidth = 1
+        cancelButton.layer.borderColor = UIColor.black.cgColor
+        cancelButton.setTitle(cancelButtonText, for: .normal)
+    }
+    
+    func setOganisationInfo() {
+        setLogoImageView()
+        setDynamicTextInfos()
+    }
+    
+    fileprivate func setLogoImageView() {
+        // Our logo image
+        let modifier = AnyModifier { request in
+            var r = request
+            r.setValue("Bearer \(UserInfo.currentUser()?.token ?? "")", forHTTPHeaderField: "Authorization")
+            if let tokendata = BBConsentKeyChainUtils.load(key: "BBConsentApiKey") {
+                let token = String(data: tokendata, encoding: .utf8) ?? ""
+                r.setValue("ApiKey \(token)", forHTTPHeaderField: "Authorization")
+                r.setValue(BBConsentPrivacyDashboardiOS.shared.userId ?? "", forHTTPHeaderField:  "X-ConsentBB-IndividualId")
+            }
+            return r
+        }
+        
+        self.ourLogoImageView.image = UIImage(named: Constant.Images.defaultCoverImage)
+        let logoImageUrl = URL(string: (organizationData?.logoImageURL ?? ""))
+        let placeholder = UIImage(named: Constant.Images.defaultCoverImage, in: Constant.getResourcesBundle(vc: BBConsentBaseViewController().classForCoder), compatibleWith: nil)
+        self.ourLogoImageView.kf.setImage(with: logoImageUrl, placeholder: placeholder, options: [.requestModifier(modifier)])
+        
+        if let url = theirLogoImageUrl {
+            self.thierLogoImageView.isHidden = false
+            self.betweenLogosImageView.isHidden = false
+            let logoImageUrl = URL(string: (url))
+            self.thierLogoImageView.kf.setImage(with: logoImageUrl)
+        } else {
+            self.thierLogoImageView.isHidden = true
+            self.betweenLogosImageView.isHidden = true
+        }
+    }
+    
+    fileprivate func setDynamicTextInfos() {
+        // First label
+        let boldAttribute = [NSAttributedString.Key.foregroundColor: UIColor.black, NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 17)]
+        let normalAttribute = [NSAttributedString.Key.foregroundColor: UIColor.black, NSAttributedString.Key.font: UIFont.systemFont(ofSize: 17)]
+        
+        let partOne = NSMutableAttributedString(string: organizationData?.name ?? "", attributes: boldAttribute)
+        let partTwo = NSMutableAttributedString(string: " wants to access the following data from ", attributes: normalAttribute)
+        partOne.append(partTwo)
+        
+        var partThree = NSMutableAttributedString()
+        var partFour = NSMutableAttributedString()
+        if let name = theirOrgName {
+            partThree = NSMutableAttributedString(string: name, attributes: boldAttribute)
+            partFour = NSMutableAttributedString(string: " for user", attributes: normalAttribute)
+        } else {
+            partThree = NSMutableAttributedString(string: "the user", attributes: normalAttribute)
+        }
+      
+        partThree.append(partFour)
+        partOne.append(partThree)
+        textLabelOne.attributedText = partOne
+        // Second label
+        textLabelTwo.text = "By clicking Authorise, \(organizationData?.name ?? "") App will be able to read the following data attributes:"
+        // Third label
+        textLabelThree.text = "Make sure that you trust \(organizationData?.name ?? "")"
+        // Data agreement & Terms of service textview
+        let normalAttributeTwo = [NSAttributedString.Key.foregroundColor: UIColor.black, NSAttributedString.Key.font: UIFont.systemFont(ofSize: 15)]
+        let linkAttribute = [NSMutableAttributedString.Key.foregroundColor: UIColor.systemBlue, NSAttributedString.Key.font: UIFont.systemFont(ofSize: 15)]
+        let dataAgreementTextOne = NSMutableAttributedString(string: "You may be sharing sensitive info with this site or app. See the ", attributes: normalAttributeTwo)
+        let dataAgreementTextTwo = NSMutableAttributedString(string: "data agreement details ", attributes: linkAttribute)
+        let dataAgreementTextThree = NSMutableAttributedString(string: "and ", attributes: normalAttributeTwo)
+        let dataAgreementTextFour = NSMutableAttributedString(string: termsOfServiceText ?? "", attributes: linkAttribute)
+        dataAgreementTextOne.append(dataAgreementTextTwo)
+        dataAgreementTextOne.append(dataAgreementTextThree)
+        dataAgreementTextOne.append(dataAgreementTextFour)
+        
+        _ = dataAgreementTextOne.setSubstringAsLink(substring: "data agreement details", linkURL: "dataAgreement://")
+        _ = dataAgreementTextOne.setSubstringAsLink(substring: termsOfServiceText ?? "", linkURL: termsOFServiceUrl ?? "")
+        dataAgreementTextView.delegate = self
+        dataAgreementTextView.attributedText = dataAgreementTextOne
+        dataAgreementTextView.isSelectable = true
+        dataAgreementTextView.isEditable = false
+        dataAgreementTextView.isUserInteractionEnabled = true
+    }
+    
+    func createAttributesView() {
+        let dataAttrubutes = organizationDetailsData?.purposeConsents?.filter({ $0.iD == dataAgreementId })
+        if let someArray = dataAttrubutes?.map({ $0.name }) {
+            for nValue in someArray {
+                let label = PaddingLabel()
+                label.edgeInset = UIEdgeInsets(top: 5, left: 10, bottom: 5, right: 10)
+                label.attributedText = NSAttributedString(string: nValue ?? "", attributes:
+                                                            [.underlineStyle: NSUnderlineStyle.single.rawValue])
+                label.textColor = .black
+                labelStackView.addArrangedSubview(label)
+            }
+            labelStackView.backgroundColor = .lightGray.withAlphaComponent(0.2)
+            labelStackView.layer.cornerRadius = 10
+        }
+    }
+    
+    // MARK: IBActions
+    @IBAction func tapOnCancel(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+    
+    @IBAction func tapOnAuthoriseButton(_ sender: Any) {
+        
+    }
+    
+    // MARK: API Calls
+    func callOrganizationApi(){
+        self.addLoadingIndicator()
+        let serviceManager = OrganisationWebServiceManager()
+        serviceManager.managerDelegate = self
+        serviceManager.getOrganization(orgId: "")
+    }
+    
+    func callOrganisationDetailsApi(){
+        let serviceManager = OrganisationWebServiceManager()
+        serviceManager.managerDelegate = self
+        serviceManager.getOrganisationDetails(orgId: BBConsentPrivacyDashboardiOS.shared.orgId ?? "")
+    }
+    
+    // MARK: Delegate methods
+    func didFinishTask(from manager: AnyObject, response: (data: RestResponse?, error: String?)) {
+        if response.error != nil {
+            if let serviceManager = manager as? OrganisationWebServiceManager {
+                if serviceManager.serviceType == .Organization {
+                    
+                }
+            }
+            showErrorAlert(message: (response.error)!)
+            return
+        }
+        
+        if let serviceManager = manager as? OrganisationWebServiceManager {
+            if serviceManager.serviceType == .Organization {
+                if let data = response.data?.responseModel as? Organization {
+                    organizationData = data
+                    setOganisationInfo()
+                }
+            } else if serviceManager.serviceType == .OrgDetails {
+                if let data = response.data?.responseModel as? OrganisationDetails {
+                    organizationDetailsData = data
+                    createAttributesView()
+                    self.removeLoadingIndicator()
+                }
+            }
+        }
+    }
+    
+    func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange) -> Bool {
+        if (URL.absoluteString == "dataAgreement://") {
+            debugPrint("Navigate to data agreement detail screen!")
+        } else if (URL.absoluteString == termsOFServiceUrl) {
+            let url = Foundation.URL(string: termsOFServiceUrl ?? "")!
+            UIApplication.shared.open(url)
+        }
+        return false
+    }
+}
+
+// Padding Label
+class PaddingLabel: UILabel {
+    var edgeInset: UIEdgeInsets = .zero
+    override func drawText(in rect: CGRect) {
+        let insets = UIEdgeInsets.init(top: edgeInset.top, left: edgeInset.left, bottom: edgeInset.bottom, right: edgeInset.right)
+        super.drawText(in: rect.inset(by: insets))
+    }
+
+    override var intrinsicContentSize: CGSize {
+        let size = super.intrinsicContentSize
+        return CGSize(width: size.width + edgeInset.left + edgeInset.right, height: size.height + edgeInset.top + edgeInset.bottom)
+    }
+}
--- PrivacyDashboardiOS/Example/PrivacyDashboardiOS/ViewController.swift
+++ PrivacyDashboardiOS/Example/PrivacyDashboardiOS/ViewController.swift
@@ -16,13 +16,23 @@ class ViewController: UIViewController {
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        PrivacyDashboard.showPrivacyDashboard(withApiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJTY29wZXMiOlsic2VydmljZSJdLCJPcmdhbmlzYXRpb25JZCI6IjY1MjY1Nzk2OTM4MGYzNWZhMWMzMDI0NSIsIk9yZ2FuaXNhdGlvbkFkbWluSWQiOiI2NTI2NTc5NjkzODBmMzVmYTFjMzAyNDMiLCJleHAiOjE3MDA3MjkxOTF9.2rkHNiLDjQi8WOy4CWn96sMBx8KkvFCUMU0Xe6oXNbY",
-                                              withUserId: "65378403b3f442eb9381b38d",
-                                              withOrgId: "64f09f778e5f3800014a879a",
-                                              withBaseUrl: "https://staging-consent-bb-api.igrant.io/v2",
-                                              turnOnAskme: false,
-                                              turnOnUserRequest: false,
-                                              turnOnAttributeDetail: false)
+//        PrivacyDashboard.showPrivacyDashboard(withApiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJTY29wZXMiOlsic2VydmljZSJdLCJPcmdhbmlzYXRpb25JZCI6IjY1MjY1Nzk2OTM4MGYzNWZhMWMzMDI0NSIsIk9yZ2FuaXNhdGlvbkFkbWluSWQiOiI2NTI2NTc5NjkzODBmMzVmYTFjMzAyNDMiLCJleHAiOjE3MDA3MjkxOTF9.2rkHNiLDjQi8WOy4CWn96sMBx8KkvFCUMU0Xe6oXNbY",
+//                                              withUserId: "65378403b3f442eb9381b38d",
+//                                              withOrgId: "64f09f778e5f3800014a879a",
+//                                              withBaseUrl: "https://staging-consent-bb-api.igrant.io/v2",
+//                                              turnOnAskme: false,
+//                                              turnOnUserRequest: false,
+//                                              turnOnAttributeDetail: false)
+        
+        PrivacyDashboard.showDataSharingUI(apiKey:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJTY29wZXMiOlsic2VydmljZSJdLCJPcmdhbmlzYXRpb25JZCI6IjY1MjY1Nzk2OTM4MGYzNWZhMWMzMDI0NSIsIk9yZ2FuaXNhdGlvbkFkbWluSWQiOiI2NTI2NTc5NjkzODBmMzVmYTFjMzAyNDMiLCJleHAiOjE3MDA3MjkxOTF9.2rkHNiLDjQi8WOy4CWn96sMBx8KkvFCUMU0Xe6oXNbY",
+                                           userId: "65378403b3f442eb9381b38d",
+                                           baseUrlString: "https://staging-consent-bb-api.igrant.io/v2",
+                                           dataAgreementId: "654cf08a9684ed907ce07c26",
+                                           organisationName: "My company",
+                                           organisationLogoImageUrl: "https://picsum.photos/id/1/200/300",
+                                           termsOfServiceText: "Terms of service.",
+                                           termsOfServiceUrl: "http://google.com",
+                                           cancelButtonText: "Cancel")
     }
 
     override func didReceiveMemoryWarning() {
