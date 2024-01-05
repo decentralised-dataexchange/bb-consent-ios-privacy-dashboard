ec93cb8df2c8134e566c15a8831d8ec8aebe7c1f
Mumthasir mohammed <>
2024-01-05 20:27:57 +0530

    Add #175: Optimisation on optin and optout call

---
+++ PrivacyDashboardiOS/Classes/Model/Organisation/RecordsModel.swift
@@ -0,0 +1,38 @@
+//
+//  RecordsModel.swift
+//  PrivacyDashboardiOS
+//
+//  Created by Mumthasir mohammed on 05/01/24.
+//
+
+import Foundation
+
+// MARK: - Records
+class RecordsModel: Codable {
+    let consentRecords: [ConsentRecordModel]
+    let pagination: RecordsPagination
+}
+
+// MARK: - ConsentRecord
+class ConsentRecordModel: Codable {
+    let id, dataAgreementID, dataAgreementRevisionID, dataAgreementRevisionHash: String
+    let individualID: String
+    var optIn: Bool
+    let state, signatureID: String
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case dataAgreementID = "dataAgreementId"
+        case dataAgreementRevisionID = "dataAgreementRevisionId"
+        case dataAgreementRevisionHash
+        case individualID = "individualId"
+        case optIn, state
+        case signatureID = "signatureId"
+    }
+}
+
+// MARK: - Pagination
+class RecordsPagination: Codable {
+    let currentPage, totalItems, totalPages, limit: Int
+    let hasPrevious, hasNext: Bool
+}
